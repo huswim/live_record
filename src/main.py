a6e3cc6393cbb802b6c3a8a3bad31a92416517c0
Hyeonuk Shin <haruswim@hanyang.ac.kr>
2023-12-01 22:22:05 +0900

    Add streamer IDs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from Recorder import RecorderManager
+from SQLManager import SQLManager
+
+streamer_id_list: list[str] = [
+    "vo_ine",
+    "jingburger",
+    "lilpaaaaaa",
+    "cotton__123",
+    "gosegugosegu",
+    "viichan6",
+]
 
 
 def main():
+    sql_manager = SQLManager("streamers.db")
+    for streamer_id in streamer_id_list:
+        sql_manager.add_streamer_id(streamer_id)
+    
+
     manager = RecorderManager("streamers.db")
     manager.start()
 
